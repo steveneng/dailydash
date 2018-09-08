ba560eb117fed780f688cec6284b33d5f9d42660
Steven Eng <steveneng@Stevens-MacBook-Pro.local>
2018-09-08 09:38:08 -0400

    added more code to the body

--- src/componenets/weather.js
+++ src/componenets/weather.js
@@ -10,23 +10,53 @@ class Weather extends Component{
        }
    }
     componentDidMount(){
-        let self=this;
-        $.ajax({
-            type: "get",
-            url: "http://api.openweathermap.org/data/2.5/weather?id=524901&APPID=29353e068b18a6d5b8999d7d4aa45b7d&zip=10001,us&units=imperial",
-            success: function (response) {
-                self.setState({
-                    temperature:response.main.temp,
-                    descriptions:response.weather[0].description
-                })
-                console.log(response.weather[0].description)
-            }
-        });
+        this.getLocation();
+        // $.ajax({
+        //     type: "get",
+        //     url: "http://api.openweathermap.org/data/2.5/weather?id=524901&APPID=29353e068b18a6d5b8999d7d4aa45b7d&zip=10001,us&units=imperial",
+        //     success: function (response) {
+        //         self.setState({
+        //             temperature:response.main.temp,
+        //             descriptions:response.weather[0].description
+        //         })
+        //         console.log(response.weather[0].description)
+        //     }
+        // });
     }
 
-    hotdog(){
-        console.log("hot odg")
+    getLocation(){
+        let self=this;
+        var getPosition = function (options) {
+            return new Promise(function (resolve, reject) {
+              navigator.geolocation.getCurrentPosition(resolve, reject, options);
+            });
+          }
+          
+        getPosition()
+            .then((position) => {
+                let link= "http://api.openweathermap.org/data/2.5/weather?id=524901&APPID=29353e068b18a6d5b8999d7d4aa45b7d&lat="+parseInt(position.coords.latitude)+"&lon="+parseInt(position.coords.longitude)+"&units=imperial"
+                $.ajax({
+                    type: "get",
+                    url: link,
+                    success: function (response) {
+                        self.setState({
+                            temperature:response.main.temp,
+                            descriptions:response.weather[0].description
+                            
+                        })
+                        console.log(response)
+                        console.log(response.weather[0].description)
+                    }
+                });
+              return([position.coords.latitude,position.coords.longitude]);
+            })
+            .catch((err) => {
+              console.error(err.message);
+            });
+
     }
+
+
     render(){
         return(
             <div>
